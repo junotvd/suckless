40b20f91a4d8b5869e25ad2119d3547aba540520
junot <vdjunot@gmail.com>
2024-12-11 00:51:54 +0100

    color testing

--- dwm/config.h
+++ dwm/config.h
@@ -7,16 +7,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Iosevka Nerd Font:size=12" };
 static const char dmenufont[]       = "Iosevka Nerd Font:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_dark_gray[]       = "#222222";
+static const char col_gray[]       = "#444444";
+static const char col_light_gray[]       = "#bbbbbb";
+static const char col_white[]       = "#eeeeee";
 static const char col_blue[]        = "#2E3440"; /* nord blue */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_blue,  col_blue  },
+	[SchemeNorm] = { col_light_gray, col_dark_gray, col_gray },
+	[SchemeSel]  = { col_white, col_blue,  col_blue  },
 };
 
 /* tagging */
@@ -58,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_dark_gray, "-nf", col_light_gray, "-sb", col_blue, "-sf", col_white, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
